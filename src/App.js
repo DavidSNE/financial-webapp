3019afc88ed3906a9e363276e2607482b3428a46
David <david.sannicolas@gmail.com>
2022-03-11 22:50:05 +0100

    feature: first version of movements table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Table} from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [movements, setMovements] = useState([{
+        "amount": "David",
+        "description": "Smith",
+        "operationDate": "Great smith"
+    }, {
+        "amount": "John",
+        "description": "Crouch",
+        "operationDate": "Mr Crouch"
+    }]);
+
+    useEffect(() => {
+        getMovements(); //fetch data from api
+    }, []);
+
+    const getMovements = async () => {
+        const response = await fetch("http://localhost/movement");
+        const movements = await response.json();
+        console.log(movements);
+        setMovements(movements);
+    };
+
+
+
+
+    return (
+        <>
+            <Table striped bordered hover variant="dark" movements={movements}>
+                <thead>
+                <tr>
+                    <th>Amount</th>
+                    <th>Description</th>
+                    <th>Operation Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {movements.map(m =>
+                    <tr>
+                        <td>{m.amount}€</td>
+                        <td>{m.description}</td>
+                        <td>{m.operationDate}</td>
+                    </tr>
+                )}
+                </tbody>
+            </Table>
+        </>
+    );
 }
 
 export default App;
